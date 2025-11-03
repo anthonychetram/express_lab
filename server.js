2033f861ed59c447906aa07627c5d42acd2c0d76
anthony Chetram <anthonychetram28@gmail.com>
2025-11-03 14:03:20 -0500

    Added User routes

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Anthony is cool!');
-    res.render("index.html");
+    res.render("index.html", {user: "Anthony!"});
+});
+app.get('/users', (req, res) => {
+    res.send("User List");
+});
+app.get('/users/new', (req, res) => {
+    res.send("New User Form");
 });
 
 app.listen = (3030);
