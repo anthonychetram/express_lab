120562fd33ee2c81a65744a99a8f25899f0f254e
anthony Chetram <anthonychetram28@gmail.com>
2025-11-10 20:25:08 -0500

    Set Post Router

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const postRouter = require('./routes/posts');
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 app.get('/', (req, res) => {
     console.log('Anthony is cool!');
