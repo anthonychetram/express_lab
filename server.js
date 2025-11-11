bb1ffad1c3556fa4a48f2e40905e90ac06a6fdd1
anthony Chetram <anthonychetram28@gmail.com>
2025-11-10 20:24:43 -0500

    Post Router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require = express();
 
 const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
 
 const app = express();
 app.set('view engine', 'ejs');
