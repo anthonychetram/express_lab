7ac6918249fcd5293f9202957a2267b9a86ce579
anthony Chetram <anthonychetram28@gmail.com>
2025-11-12 13:48:10 -0500

    fixed

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,10 @@ router.get('/new', (req, res) => {
     res.send("New User Form");
 });
 //router.get('/:id', (req, res) => {res.send(`Getting User Data: ${req.params.id}`);});
-router.route('/:id').get('/:id', (req, res) => {
+router.route('/:id').get((req, res) => {
     res.send(`Getting User Data: ${req.params.id}`);
 }).delete((req, res) => {
-    
+
 }).put((req, res) => {
     res.send(`Updating User Data: ${req.params.id}`);
 });
