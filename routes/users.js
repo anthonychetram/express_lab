5f5862f9c02319226553e9cb302a8ceab5c45317
anthony Chetram <anthonychetram28@gmail.com>
2025-11-17 14:14:22 -0500

    New EJS files and forms

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,27 @@ const express = require('express');
 const router = express.router();
 
 router.get('/', (req, res) => {
-    res.send("User List");
+    res.render(`users/list`, {users: users});
 });
 router.get('/new', (req, res) => {
     res.send("New User Form");
 });
 router.post('/', (req, res) => {
-    res.send("Create New User");
+    res.send("User Created");
+    const firstName = req.body.firstName;
+    const isVallid = firstName != "";
+    if (!isVallid) {
+        console.log (`Adding User: ${name}`);
+    users.push({name: firstName});
+    console.log(`New Set of Users: ${users}`);
+    res.send ("User Created!");
+    } else {
+         console.log ("Invalid User Data");
+         res.render ("users/new", {firstName: firstName});
+    }
 });
+
+   
 
 //router.get('/:id', (req, res) => {res.send(`Getting User Data: ${req.params.id}`);});
 router.route('/:id').get((req, res) => {
--- server.js
+++ server.js
@@ -6,19 +6,24 @@ const postRouter = require('./routes/posts');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(logger);
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+//app.use(logger);
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
+
 
 app.get('/', (req, res) => {
     console.log('Anthony is cool!');
     res.render("index", {user: "Anthony!"});
 });
 
-
+app.get(`/new`, (req, res) => {
+    res.send("users/new", {firstName: "Please Enter Your Name"});
+});
 app.listen = (3030);
 
-function logger (req, res, next) {
-    console.log (`Page Accessed: ${req.originalUrl}`);
-    next();
-}
+// function logger (req, res, next) {
+//     console.log (`Page Accessed: ${req.originalUrl}`);
+//     next();
+// }
