7951121b98ee532f2b82740255cfbd95349da859
anthony Chetram <anthonychetram28@gmail.com>
2025-11-17 13:19:31 -0500

    added logger function

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const postRouter = require('./routes/posts');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(logger);
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
 
@@ -16,3 +17,8 @@ app.get('/', (req, res) => {
 
 
 app.listen = (3030);
+
+function logger (req, res, next) {
+    console.log (`Page Accessed: ${req.originalUrl}`);
+    next();
+}
